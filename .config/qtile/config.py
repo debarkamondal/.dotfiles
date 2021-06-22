9d11b11d5488896dcead38a30b9549c7ea1c70c3
dezire <debarkamondal@gmail.com>
2021-06-22 22:11:14 +0530

    regular update

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -79,12 +79,12 @@ layout_theme = {"border_width": 2,
 
 layouts = [
         # layout.Columns(border_focus_stack='#d75f5f'),
+        layout.MonadTall(**layout_theme),
         layout.Max(**layout_theme),
         # Try more layouts by unleashing below layouts.
         # layout.Stack(num_stacks=2),
         # layout.Bsp(),
         # layout.Matrix(),
-        layout.MonadTall(**layout_theme),
         layout.MonadWide(**layout_theme),
         # layout.RatioTile(),
         layout.Tile(**layout_theme),
