a671cf93c63ba3f3cd756e8862134f8f11ba32dd
Debarka Mondal <37607719+debarkamondal@users.noreply.github.com>
2022-05-04 12:57:37 +0530

    Update livewall.sh

--- .config/livewall.sh
+++ .config/livewall.sh
@@ -2,4 +2,4 @@
 
 folder="/home/dezire/Downloads/livewall"
 name=$(ls $folder/* | shuf -n1) 
-xwinwrap -fs -fdt -ni -b -nf -- mpv -wid WID --no-audio --loop $name
+xwinwrap -fs -fdt -ni -b -nf -- mpv -wid WID --no-audio --loop $name &
