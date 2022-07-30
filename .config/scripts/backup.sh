f6ab0990b7f2a2b07d01f754b1af79a973d3a152
dezire <debarkamondal@gmail.com>
2022-07-30 09:00:07 +0530

    push

--- .config/scripts/backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rsync --progress --ignore-existing -avzr dezire@pi4:/mnt/external/docker /mnt/Important/
-rsync --progress --ignore-existing -azvr  /mnt/Important/Dad dezire@pi4:/mnt/external/shared/
